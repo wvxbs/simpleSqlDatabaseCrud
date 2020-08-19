42e68d41044442eb2583839649dcb6d53a734bdf
Gabriel Ferreira <gabriel.ferreira@esx.com.br>
2020-08-19 01:34:06 -0300

    app start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simpleSqlDatabaseCrud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
